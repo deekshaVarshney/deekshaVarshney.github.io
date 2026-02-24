eda67f8bcd9fd35790a1c0f063be333051c9eb31
pooja-rani06 <poojaranilagudu06@gmail.com>
2026-02-24 16:03:59 +0000

    Deploying to gh-pages from @ deekshaVarshney/deekshaVarshney.github.io@011025d582e49aa23d9e73dca2421980d5f54bbd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,11 +457,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-ai-safety",
           title: 'AI safety',
           description: "with background image",
